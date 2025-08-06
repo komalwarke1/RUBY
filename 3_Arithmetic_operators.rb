c98179c2b011365d7233e93470ad034c52cc05e3
komalwarke1 <warkekomal04@gmail.com>
2025-08-06 17:36:43 +0530

    Arithmetic operators

---
+++ 3_Arithmetic_operators.rb
@@ -0,0 +1,14 @@
+#Arithmetic operators in ruby are used to perform mathematical operations on numbers.
+# The basic arithmetic operators in Ruby are:
+# + (addition), - (subtraction), * (multiplication), / (division), % (modulus), ** (exponentiation)
+# Example of arithmetic operations in Ruby
+a = 10
+b = 3
+
+puts a + b   # Addition => 13
+puts a - b   # Subtraction => 7
+puts a * b   # Multiplication => 30
+puts a / b   # Division (integer division) => 3
+puts a % b   # Modulus => 1
+puts a ** b  # Exponentiation => 1000
+puts "Arithmetic operations completed successfully!"
