345de5b3465ed028853b4f2f0aa732e60184df5f
komalwarke1 <warkekomal04@gmail.com>
2025-08-06 11:43:47 +0530

    print hello world

---
+++ 01_print.rb
@@ -0,0 +1,6 @@
+# For printing 'hello world' to the console
+#Difference between puts and print: 
+#puts adds a new line after the output, while print does not
+puts 'hello world'
+print 'hello world'
+puts 'hello world01'
