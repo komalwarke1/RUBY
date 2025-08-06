c931c07c1b2ada9e247be4b22f037302a034c9fb
komalwarke1 <warkekomal04@gmail.com>
2025-08-06 17:09:46 +0530

    data types

---
+++ 02_data_types.rb
@@ -0,0 +1,11 @@
+#In ruby we dont need to mention data types such as number, string, boolean etc.
+# Ruby automatically infers the data type based on the value assigned to a variable.
+num=10;
+string="here is string"
+boolean=true
+floatnum=0.89
+
+puts num
+puts string
+puts boolean
+puts floatnum
